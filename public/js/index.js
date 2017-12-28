283bb52f4b2b27ad6e71fa62318ececa0ee5cb9e
Marklar <dmitri.bilyk@gmail.com>
2017-12-28 00:51:18 -0800

    core functionality in place

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,27 @@
-$(document).ready(function(){
-  
 
+function QuoteMachine(){
+  this.quoteP = $("#quote-text").clone();
+  this.getQuote = function(){
+    $.getJSON("https://talaikis.com/api/quotes/random/",(data)=>{
+      var quote = data.quote;
+      var author = data.author;
+      var newQuoteP = this.quoteP.clone();
+      var oldQuoteP = $("#quote-text");
+      oldQuoteP.animate({marginTop : ($(oldQuoteP).outerHeight() *-1-10) + "px"},500,()=>{
+        $(oldQuoteP).remove();
+      })
+      $(".overlay").first().append(newQuoteP);
+      $(newQuoteP).children("#quote-target").html(quote);
+      $(newQuoteP).children("#author-target").html("- " + author);
+      $("#quote-div, .overlay:first").css({height:$(oldQuoteP).outerHeight() + "px"});
+      $("#quote-div, .overlay:first").animate({height:$(newQuoteP).outerHeight() + "px"},500);
+    })
+  }
+}
+
+$(document).ready(function(){
+  var quoteMachine = new QuoteMachine();
+  $("#new-quote").click((e)=>{
+    quoteMachine.getQuote();
+  }) 
 })
--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const app = express();
 const server = require("http").createServer(app);
 const pug = require("pug");
 const sass = require("node-sass-middleware");
-const fs = require("fs");
 
+//setting up pug middleware
 app.set("views","/pug");
 app.set("view engine", "pug");
+
+//this is how to add sass middleware compilation.  
+//if the html css link is "/style.css", it will look for scss file in src, and output it to dest as css.
 app.use(
   sass({
     src : path.join(__dirname, "sass"),
@@ -20,8 +23,11 @@ app.use(
 app.use("/public",express.static(path.join(__dirname,"public")));
 
 app.get("/", (req,res)=>{
+  
   var index = pug.renderFile("./pug/index.pug");
   res.send(index);
 })
+
+
 
 server.listen(3000, "localhost",()=>{console.log("up and running on 3000")});
